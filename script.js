a5dba917dc44e684059639404fab9412ddb5b779
Edward Lu <eddywerdy@gmail.com>
2016-04-26 17:21:56 -0400

    Added menu functionality.

--- script.js
+++ script.js
@@ -1,11 +1,46 @@
 $(document).ready(function() {
-	// load animations
+	// load menu animation
 	$("#header").css("margin-top", "-152px");
 	$("#header").animate({ marginTop: "-76px" }, 500);
 
+	// load blocks animation
 	$(".block").hide();
-
 	$('.block').each(function(i, e) {
     	$(this).delay(500 + 1000 * Math.random()).fadeTo(750, 1);
     });
+
+    $("#about-link").click(function() {
+    	$(".block-about").show(250);
+ 		$(".block-design").hide(250);
+ 		$(".block-project").hide(250);
+ 		$(".block-contact").hide(250);
+	});
+
+	$("#design-link").click(function() {
+    	$(".block-design").show(250);
+ 		$(".block-about").hide(250);
+ 		$(".block-project").hide(250);
+ 		$(".block-contact").hide(250);
+	});
+
+	$("#title-link").click(function() {
+    	$(".block-about").show(250);
+ 		$(".block-design").show(250);
+ 		$(".block-project").show(250);
+ 		$(".block-contact").show(250);
+	});
+
+	$("#project-link").click(function() {
+    	$(".block-project").show(250);
+ 		$(".block-about").hide(250);
+ 		$(".block-design").hide(250);
+ 		$(".block-contact").hide(250);
+	});
+
+	$("#contact-link").click(function() {
+    	$(".block-contact").show(250);
+ 		$(".block-about").hide(250);
+ 		$(".block-design").hide(250);
+ 		$(".block-project").hide(250);
+	});
 });
